2b9562cc4e3c179c5312ccf6b28feb7eadd059d2
Hugo Costa <costarafaelhugo@gmail.com>
2022-07-01 14:58:19 -0300

    All

---
+++ app/src/main/java/com/hugorafaelcosta/splashscreenandroid12/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.hugorafaelcosta.splashscreenandroid12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Thread.sleep(2000)
+        installSplashScreen()
+        setContentView(R.layout.activity_main)
+    }
+}
